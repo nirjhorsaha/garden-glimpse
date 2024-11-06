73d80bbbcace3bf438470592a80a654edbc79d40
nirjhorsaha <nirjhor.badhan25@gmail.com>
2024-11-06 15:51:21 +0600

    [Updated] global components

--- src/components/UI/Global/Container.tsx
+++
@@ -1,8 +0,0 @@
-/* eslint-disable prettier/prettier */
-export default function Container({ children }: { children: React.ReactNode }) {
-    return (
-      <div className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
-        {children}
-      </div>
-    );
-  }
