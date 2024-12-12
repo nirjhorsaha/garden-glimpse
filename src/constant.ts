917278b4a336f8286dd92c0cdd4d016e34af1366
nirjhorsaha <nirjhor.badhan25@gmail.com>
2024-12-12 09:06:46 +0600

    [Added] const file

--- src/constant.ts
+++ src/constant.ts
@@ -1,2 +1,33 @@
-/* eslint-disable prettier/prettier */
-export const protectedRoutes = ["/profile", "/admin", "/login", "/register"];
+// List of routes that necessitate user authentication to access
+export const protectedRoutes = [
+  '/profile',
+  '/profile/:page*',
+  '/post/:page*',
+  '/admin',
+  '/login',
+  '/register',
+];
+
+// define table columns value
+export const columns = [
+  { name: 'NAME', uid: 'name' },
+  { name: 'ROLE', uid: 'role' },
+  { name: 'STATUS', uid: 'status' },
+  { name: 'ACTIONS', uid: 'actions' },
+];
+
+// Define dropdown category
+export const categories = [
+  { value: 'Vegetables', label: 'Vegetables' },
+  { value: 'Flowers', label: 'Flowers' },
+  { value: 'Landscaping', label: 'Landscaping' },
+  { value: 'Succulents', label: 'Succulents' },
+  { value: 'Indoor Plants', label: 'Indoor Plants' },
+  { value: 'Others', label: 'Others' },
+];
+
+// Define motion variants
+export const variants = {
+  hidden: { opacity: 0, y: 20 },
+  visible: { opacity: 1, y: 0 },
+};
